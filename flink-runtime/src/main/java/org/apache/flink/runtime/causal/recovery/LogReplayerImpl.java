42d1dca9706f825c122c82c1da39cd34c5249598
yuta-cloud <yuta.tanikawa@sslab.ics.keio.ac.jp>
2023-08-16 10:58:30 +0000

    debug bytebuf copy

--- flink-runtime/src/main/java/org/apache/flink/runtime/causal/recovery/LogReplayerImpl.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/causal/recovery/LogReplayerImpl.java
@@ -206,19 +206,17 @@ public class LogReplayerImpl implements LogReplayer {
 			System.out.println("WaitCausalLog start");
 			try {
 				while (true) {
-					System.out.println("queue take1 length: " + queue.size());
 					ByteBuf value = queue.take();
-					System.out.println("queue take2 length: " + queue.size());
 					System.out.println("get bytebuf: " + context.vertexGraphInformation.getThisTasksVertexID().getVertexID());
 					System.out.println(value.toString());
 					short determinantVertexID = value.readShort();
 					System.out.println("bytebuf vertex ID: " + determinantVertexID);
 					if(determinantVertexID != context.vertexGraphInformation.getThisTasksVertexID().getVertexID()){
-						value.resetReaderIndex();
+						value.release();
 						continue;
 					}
 					System.out.println("Match Vertex ID");
-					System.out.println(context.vertexGraphInformation.getThisTasksVertexID().getVertexID());
+					System.out.println(value.toString());
 					lock.lock();
 					try {
 						log.writeBytes(value);

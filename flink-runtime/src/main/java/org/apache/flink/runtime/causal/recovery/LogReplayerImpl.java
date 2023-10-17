2eb1c3ad7379d2459d782d55baa6da794c59f0d1
yuta-cloud <yuta.tanikawa@sslab.ics.keio.ac.jp>
2023-10-17 11:08:43 +0000

    checkFlag non static

--- flink-runtime/src/main/java/org/apache/flink/runtime/causal/recovery/LogReplayerImpl.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/causal/recovery/LogReplayerImpl.java
@@ -68,7 +68,7 @@ public class LogReplayerImpl implements LogReplayer {
 	Determinant nextDeterminant;
 
 	private boolean done;
-	private static volatile boolean checkFlag;
+	private boolean checkFlag;
 
 	public LogReplayerImpl(ByteBuf log, RecoveryManagerContext recoveryManagerContext) {
 		this.context = recoveryManagerContext;

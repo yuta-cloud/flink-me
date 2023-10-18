65b83bcf86b233e883584489d3cf35f4099899c8
yuta-cloud <yuta.tanikawa@sslab.ics.keio.ac.jp>
2023-10-18 11:14:19 +0000

    fix server lib bug

--- flink-runtime/src/main/java/org/apache/flink/runtime/causal/determinant/MeTCPServer.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/causal/determinant/MeTCPServer.java
@@ -40,6 +40,7 @@ import org.apache.flink.shaded.netty4.io.netty.util.CharsetUtil;
 import org.apache.flink.shaded.netty4.io.netty.channel.epoll.EpollSocketChannel;
 import org.apache.flink.shaded.netty4.io.netty.channel.socket.SocketChannel;
 import org.apache.flink.shaded.netty4.io.netty.channel.epoll.EpollEventLoopGroup;
+import org.apache.flink.shaded.netty4.io.netty.channel.epoll.EpollChannelOption;
 import java.util.concurrent.*;
 
 public class MeTCPServer{
